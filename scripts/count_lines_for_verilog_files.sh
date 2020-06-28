9ebe363eef9398933c05bd637b84c0d71b3585bb
freshman365 <50549453+freshman365@users.noreply.github.com>
2020-06-28 15:18:33 +0800

    Update count_lines_for_verilog_files.sh
    Update comments part calculation.

--- scripts/count_lines_for_verilog_files.sh
+++ scripts/count_lines_for_verilog_files.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+find ./ -name "*.v" -or -name "*.sv" |xargs wc -l
 echo "Total line number is: "
 find ./ -name "*.v" -or -name "*.sv" |xargs cat|wc -l
 echo "Total line number exclude comments and blank line is: "
-find ./ -name "*.v" -or -name "*.sv" |xargs cat|grep -v -e ^$ -e ^\s*\/\/.*$|wc -l
+find ./ -name "*.v" -or -name "*.sv" |xargs cat|grep -v -e ^[[:space:]]*$ -e ^[[:space:]]*\/\/.*$|wc -l 
+find ./ -name "*.v" -or -name "*.sv" |xargs cat|grep -e ^[[:space:]]*$ -e ^[[:space:]]*\/\/.*$ > 1.txt  # |wc -l 
+find ./ -name "*.v" -or -name "*.sv" |xargs cat|grep -v -e ^[[:space:]]*$ -e ^[[:space:]]*\/\/.*$ > 2.txt  # |wc -l 
+echo "comments percentage is "
+a=$(find ./ -name "*.v" -or -name "*.sv" |xargs cat|grep -e ^[[:space:]]*\/\/.*$|wc -l)
+b=$(find ./ -name "*.v" -or -name "*.sv" |xargs cat|wc -l)
+# tmp=`expr $a / $b`
+# echo $tmp
+echo "`bc <<< "scale=2; 100*$a/$b"`%"
